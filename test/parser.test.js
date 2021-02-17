ce5aabc062bc2074270057a715719ed2e3bc3c7a
zandickens <zan@zandickens.com>
2021-02-16 20:55:50 -0800

    Tests updated

--- test/parser.test.js
+++ test/parser.test.js
@@ -2,22 +2,22 @@ import assert from "assert"
 import util from "util"
 import parse from "../src/parser.js"
 
-const source = `let dozen = 1 * (0 ** sqrt 101.3)
+const source = `let dozen = 1 * (0 + sqrt 101.3)
   let y = dozen == 0    // TADA 🥑
-  dozen = 0 / y
+  dozen = 0 ** y
   print abs dozen //`
 
 const expectedAst = `   1 | Program statements=[#2,#6,#9,#13]
    2 | Variable name='dozen' initializer=#3
    3 | BinaryExpression op='*' left=1 right=#4
-   4 | BinaryExpression op='**' left=0 right=#5
+   4 | BinaryExpression op='+' left=0 right=#5
    5 | UnaryExpression op='sqrt' operand=101.3
    6 | Variable name='y' initializer=#7
    7 | BinaryExpression op='==' left=#8 right=0
    8 | IdentifierExpression name='dozen'
    9 | Assignment target=#10 source=#11
   10 | IdentifierExpression name='dozen'
-  11 | BinaryExpression op='/' left=0 right=#12
+  11 | BinaryExpression op='**' left=0 right=#12
   12 | IdentifierExpression name='y'
   13 | PrintStatement argument=#14
   14 | UnaryExpression op='abs' operand=#15
